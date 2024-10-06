92b4a7f121914dceaf15c12ddbb5c2f32d7aa0ab
DANG MINH HAI <v-haidang@outlook.com>
2024-10-06 16:18:28 +0700

    Multi langguage Location done

---
+++ ServerLibrary/Data/Migrations/20241006074222_Second_Time.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerLibrary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second_Time : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ResourceValue",
+                table: "LocaleStringResources",
+                newName: "Value");
+
+            migrationBuilder.RenameColumn(
+                name: "ResourceKey",
+                table: "LocaleStringResources",
+                newName: "Key");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Value",
+                table: "LocaleStringResources",
+                newName: "ResourceValue");
+
+            migrationBuilder.RenameColumn(
+                name: "Key",
+                table: "LocaleStringResources",
+                newName: "ResourceKey");
+        }
+    }
+}
